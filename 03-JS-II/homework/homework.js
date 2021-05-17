06f2d0d4928504a273778bd127ce8537d4e86b4a
unknown <ulisesaxel@gmail.com>
2021-05-16 23:48:14 -0300

    CP commiteado por ulisesaxel a las 1621219694764

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -188,7 +188,7 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  var arreglo = new Array(11);
+  var arreglo = new Array(11); //Declaracion
   for(i = 0; i <= 10; i++){
     arreglo[i] = i * 6;
   }
